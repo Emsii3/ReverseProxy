bdc6616915d62b3baae701a406d45e05bff8fd5c
Emsii3 <iron88895@gmail.com>
2026-09-24 21:27:32 +0200

    fix: cancel context on empty backends to prevent localhost:80 routing

--- director.go
+++ director.go
@@ -15,7 +15,6 @@ type customDirector struct {
 }
 
 func (c *customDirector) Direct(req *http.Request) {
-	// adding headers
 	clientAddr, _, err := net.SplitHostPort(req.RemoteAddr)
 	if err == nil {
 		req.Header.Set("X-Forwarded-For", clientAddr)
@@ -23,24 +22,19 @@ func (c *customDirector) Direct(req *http.Request) {
 		req.Header.Set("X-Real-IP", clientAddr)
 	}
 
-	//load balancer using round-robin
 	backends := c.aliveBackends.Load()
-	var idx int
-	var count uint64
-
-	if backends != nil && int64(len(*backends)) > 0 {
-		count = c.backendCounter.Add(1)
-		idx = int(int64(count) % int64(len(*backends)))
-	} else {
+	if backends == nil || len(*backends) == 0 {
 		ctx, cancel := context.WithCancel(req.Context())
 		cancel()
 		*req = *req.WithContext(ctx)
 		return
 	}
+
 	c.originalDirector(req)
+	count := c.backendCounter.Add(1)
+	idx := int(int64(count) % int64(len(*backends)))
 	target := (*backends)[idx]
 	req.URL.Scheme = target.Scheme
-	req.Host = target.Host
 	req.URL.Host = target.Host
 	req.Host = target.Host
 }
